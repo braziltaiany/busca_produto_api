8d64e1c8a16a4b2c6fc3f5ed73513720b512fd9b
Taiany <braziltaiany@outlook.com>
2020-10-18 17:25:24 -0300

    cors

--- node-api/server.js
+++ node-api/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
+
 require('dotenv').config(); //config dotenv - variavel de ambiente
 
 //iniciando o app
@@ -8,6 +10,9 @@ const app = express();
 
 //permissão para criação de arquivos json para api rest
 app.use(express.json());
+
+//permissão de acesso para os dominios, habilitado para todas as origens
+app.use(cors());
 
 //iniciando o DATABASE
 mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true });
